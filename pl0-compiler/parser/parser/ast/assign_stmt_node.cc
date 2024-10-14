76a18982826afc03a420319d232f540b9d4d1170
DanielH4 <danne.halvarsson@gmail.com>
2024-10-14 21:05:24 +0200

    update assign statement node with missing expressions

--- pl0-compiler/parser/parser/ast/assign_stmt_node.cc
+++ pl0-compiler/parser/parser/ast/assign_stmt_node.cc
@@ -1,6 +1,13 @@
 #include "parser/ast/assign_stmt_node.hh"
 
+#include "parser/ast/division_expr_node.hh"
+#include "parser/ast/id_expr_node.hh"
+#include "parser/ast/input_expr_node.hh"
 #include "parser/ast/int_expr_node.hh"
+#include "parser/ast/minus_expr_node.hh"
+#include "parser/ast/multiplication_expr_node.hh"
+#include "parser/ast/negative_expr_node.hh"
+#include "parser/ast/plus_expr_node.hh"
 
 namespace pl0c
 {
@@ -37,6 +44,22 @@ bool operator==(const AssignStmtNode &lhs, const AssignStmtNode &rhs)
 
     switch (lhs.exprNode_->getType())
     {
+    case ExprNodeType::DIVISION_EXPR: {
+      const auto lhsDivisionExpr =
+          std::dynamic_pointer_cast<DivisionExprNode>(lhs.exprNode_);
+      const auto rhsDivisionExpr =
+          std::dynamic_pointer_cast<DivisionExprNode>(rhs.exprNode_);
+
+      return (*lhsDivisionExpr == *rhsDivisionExpr);
+    }
+    case ExprNodeType::ID_EXPR: {
+      const auto lhsIdExpr =
+          std::dynamic_pointer_cast<IdExprNode>(lhs.exprNode_);
+      const auto rhsIdExpr =
+          std::dynamic_pointer_cast<IdExprNode>(rhs.exprNode_);
+
+      return (*lhsIdExpr == *rhsIdExpr);
+    }
     case ExprNodeType::INT_EXPR: {
       const auto lhsIntExpr =
           std::dynamic_pointer_cast<IntExprNode>(lhs.exprNode_);
@@ -44,6 +67,46 @@ bool operator==(const AssignStmtNode &lhs, const AssignStmtNode &rhs)
           std::dynamic_pointer_cast<IntExprNode>(rhs.exprNode_);
 
       return (*lhsIntExpr == *rhsIntExpr);
+    }
+    case ExprNodeType::INPUT_EXPR: {
+      const auto lhsInputExpr =
+          std::dynamic_pointer_cast<InputExprNode>(lhs.exprNode_);
+      const auto rhsInputExpr =
+          std::dynamic_pointer_cast<InputExprNode>(rhs.exprNode_);
+
+      return (*lhsInputExpr == *rhsInputExpr);
+    }
+    case ExprNodeType::MINUS_EXPR: {
+      const auto lhsMinusExpr =
+          std::dynamic_pointer_cast<MinusExprNode>(lhs.exprNode_);
+      const auto rhsMinusExpr =
+          std::dynamic_pointer_cast<MinusExprNode>(rhs.exprNode_);
+
+      return (*lhsMinusExpr == *rhsMinusExpr);
+    }
+    case ExprNodeType::MULTIPLICATION_EXPR: {
+      const auto lhsMultiplicationExpr =
+          std::dynamic_pointer_cast<MultiplicationExprNode>(lhs.exprNode_);
+      const auto rhsMultiplicationExpr =
+          std::dynamic_pointer_cast<MultiplicationExprNode>(rhs.exprNode_);
+
+      return (*lhsMultiplicationExpr == *rhsMultiplicationExpr);
+    }
+    case ExprNodeType::NEGATIVE_EXPR: {
+      const auto lhsNegativeExpr =
+          std::dynamic_pointer_cast<NegativeExprNode>(lhs.exprNode_);
+      const auto rhsNegativeExpr =
+          std::dynamic_pointer_cast<NegativeExprNode>(rhs.exprNode_);
+
+      return (*lhsNegativeExpr == *rhsNegativeExpr);
+    }
+    case ExprNodeType::PLUS_EXPR: {
+      const auto lhsPlusExpr =
+          std::dynamic_pointer_cast<PlusExprNode>(lhs.exprNode_);
+      const auto rhsPlusExpr =
+          std::dynamic_pointer_cast<PlusExprNode>(rhs.exprNode_);
+
+      return (*lhsPlusExpr == *rhsPlusExpr);
     }
     default: {
       return false;
